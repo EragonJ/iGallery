4eea13b7ccf3dbc6788b7a962212961c98cb99f9
EragonJ <jack.xxlong@gmail.com>
2010-06-14 15:26:22 +0800

    test

---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  iGallery
+//
+//  Created by EragonJ on 2010/6/14.
+//  Copyright Hax4 2010. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
